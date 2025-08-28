6f3df8049faee815cb1c8ef360d609bc9941a729
orekhov-vitaly <orekhov.vitaly@gmail.com>
2025-08-28 20:54:20 +0200

    Task1 Fix Task2

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -1,5 +1,6 @@
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("Это первая версия");
+        System.out.println("Обновление класса Task1");
     }
 }
